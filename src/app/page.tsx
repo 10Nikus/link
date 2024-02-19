fde014efd97683b1a3012c068091f3775ea91ae9
10Nikus <kijasnikodem@gmail.com>
2024-02-19 18:51:26 +0100

    Update page layout and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>working</h1>;
+  return (
+    <>
+      <h1 className="headingMedium">Your Link will be here</h1>
+      <h1 className="headingSmall">not bold</h1>
+      <h1 className="bodyMedium">body mid</h1>
+      <h1 className="bodySmall">body sm</h1>
+    </>
+  );
 }
