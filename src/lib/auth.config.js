1b3f5b5a478eb0d4fe79b836dbe40b3853a2a299
10Nikus <kijasnikodem@gmail.com>
2024-02-29 11:00:02 +0100

    Add Github button to preview page

---
+++ src/lib/auth.config.js
@@ -0,0 +1,11 @@
+// export const authConfig = {
+//   callbacks: {
+//     pages: {
+//       signIn: "/login",
+//     },
+//     providers: [],
+//     authorized({ auth, request }) {
+//       return false;
+//     },
+//   },
+// };
