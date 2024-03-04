986e980ae8aaecf33eadbac6aca5af756a1e4162
10Nikus <kijasnikodem@gmail.com>
2024-03-04 11:33:29 +0100

    Add dropdown menu for selecting link type in LinkForm component

--- src/components/form/LinkForm/LinkForm.tsx
+++ src/components/form/LinkForm/LinkForm.tsx
@@ -12,14 +12,31 @@ export default function LinkForm({
   type: string;
 }) {
   const [state, setState] = useState({ link, type });
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     setState({ ...state, [e.target.name]: e.target.value });
   };
 
   return (
     <div>
       <form>
-        <input name="type" value={state.type} onChange={handleChange} />
+        <select name="type" value={state.type} onChange={handleChange}>
+          <option value="github">Github</option>
+          <option value="frontend Mentor">Frontend Mentor</option>
+          <option value="twitter">Twitter</option>
+          <option value="linkedin">Linkedin</option>
+          <option value="youtube">Youtube</option>
+          <option value="facebook">Facebook</option>
+          <option value="twitch">Twitch</option>
+          <option value="dev.to">Dev.to</option>
+          <option value="codewars">Codewars</option>
+          <option value="codepen">Codepen</option>
+          <option value="freeCodeCamp">freeCodeCamp</option>
+          <option value="gitlab">Gitlab</option>
+          <option value="hashnode">Hashnode</option>
+          <option value="stackoverflow">Stackoverflow</option>
+        </select>
         <input
           name="link"
           type="text"
